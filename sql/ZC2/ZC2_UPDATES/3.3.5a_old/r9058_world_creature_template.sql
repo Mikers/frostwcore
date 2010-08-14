9ba2e9904498a6fcab9db25cb88d142e12efaa43
Mikers <Mikers@localhost>
2010-08-14 23:13:52 +0400

    +++ cleanup

--- sql/ZC2/ZC2_UPDATES/3.3.5a_old/r9058_world_creature_template.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `creature_template` SET `mechanic_immune_mask` = 650854239 WHERE `entry` = 32865;
--- sql/ZC2/ZDB/CUSTOM/INSTANCE_ALL.sql
+++
@@ -1,3 +0,0 @@
--- Delete Lich King achievements from characters
-DELETE FROM `character_achievement` WHERE `achievement` IN (4583,4601,4621,4581,4622,4576,4530,4597,4584);
-DELETE FROM `character_achievement_progress` WHERE `criteria` IN (4583,4601,4621,4581,4622,4576,4530,4597,4584);
